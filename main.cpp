3c4227f4a4fe2009afdb557e999ce0c83ca72398
hyagoshodan <hyagodejesus@outlook.com>
2022-01-17 12:33:48 -0300

    Primeiro commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "editordetextos.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    EditorDeTextos w;
+    w.show();
+    return a.exec();
+}
